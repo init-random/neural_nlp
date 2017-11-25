b945f28bd6dd3e611c5fd37c4fe9ac688c2aa043
init-random <chewning_keith@yahoo.com>
2017-11-24 23:43:01 -0500

    piped map to itertoolz

--- neural_nlp/toolz/itertoolz.py
+++ neural_nlp/toolz/itertoolz.py
@@ -7,3 +7,12 @@ def unzip(seq):
     t = z.pipe(range(l), z.map(lambda i: z.pipe(seq, z.pluck(i), iter)), tuple)
     return t
 
+
+def piped_map(seq, *funcs):
+    return z.map(lambda v: z.pipe(v, *funcs), seq)
+
+
+@z.curry
+def split(delim, s):
+    s.split(delim)
+
