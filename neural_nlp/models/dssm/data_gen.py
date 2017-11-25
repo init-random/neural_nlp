edc012dd71dcbc6afdc13c9d244abd1ea161700a
init-random <chewning_keith@yahoo.com>
2017-11-24 23:23:32 -0500

    mv to models

--- neural_nlp/models/dssm/data_gen.py
+++ neural_nlp/models/dssm/data_gen.py
@@ -1,5 +1,27 @@
+from scipy.sparse import coo_matrix
 
-ss = ['this place is crazy', 'the coffee smells good', 'some bears are like honey!']
+class LilIndexer:
+    def __init__(self, sparse_mtrx, index_to_dense=True):
+        self.sparse_mtrx = sparse_mtrx
+        self.index_to_dense = index_to_dense
+        self.shape = sparse_mtrx.shape
+
+    def __getitem__(self, item):
+        return self.sparse_mtrx[item].todense()
+
+    def __call__(self, *args, **kwargs):
+        return True
+
+
+
+def line_text(s):
+    t = s.lower().strip().split('\t')
+    print(len(t))
+    if len(t) == 1: return None
+    if len(t) == 2:
+        return t[1]
+    return '%s %s' % (t[1], t[2])
+
 
 
 def char_hasher(sentence):
@@ -28,6 +50,9 @@ vocab_len = len(_words)
 vocab_map = {w: idx for idx, w in enumerate(_words)}
 del _words
 
+ss = open('/home/k/data/dssm/curation_content.txt', 'r').readlines()
+ss = [line_text(s) for s in ss if line_text(s) is not None]
+
 v_set = set()
 for s in ss:
     for token in char_hasher(s):
@@ -47,7 +72,9 @@ for w, r_idx in vocab_map.items():
         col.append(hasher_map[t])
         val.append(1)
 
-from scipy.sparse import coo_matrix
 
 # this is the embedding weight matrix
-embeddings = coo_matrix((val, (row, col)), shape=(vocab_len, hasher_len)).toarray()
+embeddings = LilIndexer(coo_matrix((val, (row, col)), shape=(vocab_len, hasher_len)).tolil().astype('int32'))
+
+
+
--- neural_nlp/models/dssm/network.py
+++ neural_nlp/models/dssm/network.py
@@ -2,25 +2,80 @@ import numpy as np
 import lasagne
 import theano.tensor as T
 import theano
+from scipy.sparse import coo_matrix
+
+
+
+# class DotLayer(lasagne.layers.Layer):
+#     def __init__(self, incoming, num_units, W=lasagne.init.Normal(0.01), **kwargs):
+#         super(DotLayer, self).__init__(incoming, **kwargs)
+#         num_inputs = self.input_shape[1]
+#         self.num_units = num_units
+#         self.W = self.add_param(W, (num_inputs, num_units), name='W')
+#
+#     def get_output_for(self, input, **kwargs):
+#         return T.dot(input, self.W)
+#
+#     def get_output_shape_for(self, input_shape):
+#         return (input_shape[0], self.num_units)
+
+
+
+
+class CosineMergeLayer(lasagne.layers.MergeLayer):
+    def __init__(self, incomings, **kwargs):
+        super(CosineMergeLayer, self).__init__(incomings, **kwargs)
+
+
+    def get_output_for(self, inputs, **kwargs):
+        a = inputs[0]
+        b = inputs[1]
+        a /= T.sum(a * a, axis=1, keepdims=True)
+        b /= T.sum(b * b, axis=1, keepdims=True)
+        return T.sum(a * b, axis=1, keepdims=True)
+
+    def get_output_shape_for(self, input_shape):
+        return (input_shape[0][0], 1) # dot prod -> 1 dim
+
 
 ########################################################################################################################
 # network
 ########################################################################################################################
 
+batch_size = 2 #50
+branches = 1
+char_hasher_dim = 5 #19000
+hidden_units = 300
+semantic_feature_units = 7
+
 input_var = T.imatrix().astype('int32')
+input_var = T.itensor3().astype('int32')
 
 x = T.imatrix()
-l_in = lasagne.layers.InputLayer((None, ))
-W = np.array([1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1]).reshape((5, 3)).astype('float32')
-l1 = lasagne.layers.EmbeddingLayer(l_in, input_size=5, output_size=3, W=W)
-reshape = lasagne.layers.ReshapeLayer(l1, ([0], 1, -1, 3))
-pool = lasagne.layers.MaxPool2DLayer(reshape,
-                              pool_size=(5, 1),
-                              pad=(0, 0),
-                              ignore_border=False)
-output = lasagne.layers.get_output(l1, x)
-f = theano.function([x], output)
-x_test = np.array([[0, 2, 4, 1, 2]]).astype('int32')
+# (batch, branches+1, char_hasher_dim): +1 because query is also a branch
+l_in = lasagne.layers.InputLayer((batch_size, branches+1, char_hasher_dim))
+
+q_in = lasagne.layers.SliceLayer(l_in, indices=0, axis=1)
+# fork here to create branches
+d1_in = lasagne.layers.SliceLayer(l_in, indices=1, axis=1)
+
+
+h1 = lasagne.layers.DenseLayer(q_in, num_units=hidden_units, nonlinearity=lasagne.nonlinearities.tanh)
+h2 = lasagne.layers.DenseLayer(h1, num_units=hidden_units, nonlinearity=lasagne.nonlinearities.tanh)
+query_feature = lasagne.layers.DenseLayer(h2, num_units=semantic_feature_units, nonlinearity=lasagne.nonlinearities.tanh)
+
+
+
+d1_h1 = lasagne.layers.DenseLayer(d1_in, num_units=hidden_units, nonlinearity=lasagne.nonlinearities.tanh)
+d1_h2 = lasagne.layers.DenseLayer(h1, num_units=hidden_units, nonlinearity=lasagne.nonlinearities.tanh)
+d1_feature = lasagne.layers.DenseLayer(h2, num_units=semantic_feature_units, nonlinearity=lasagne.nonlinearities.tanh)
+
+dm = CosineMergeLayer([query_feature, d1_feature])
+
+
+output = lasagne.layers.get_output(dm, input_var)
+f = theano.function([input_var], output)
+x_test = np.array([[[0, 2, 4, 1, 2],[0, 3, 4, 1, 2]], [[0, 2, 3, 1, 2],[3, 3, 4, 1, 2]]]).astype('int32')
 f(x_test)
 
 output = lasagne.layers.get_output(pool, x)
@@ -31,7 +86,7 @@ f(x_test)
 
 l_in = lasagne.layers.InputLayer((3, 5), input_var=input_var)
 embedding = lasagne.layers.EmbeddingLayer(l_in, input_size=5, #self.vocab_size,
-                                          output_size=3, W=embedding_weights)
+                                      output_size=3, W=embedding_weights)
 
 
 class NetworkGen():
