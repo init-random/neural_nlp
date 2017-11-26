124cca8381741a33368fb5bab847b2cb49fb4f5e
init-random <chewning_keith@yahoo.com>
2017-11-26 17:03:44 -0500

    documentation

--- neural_nlp/datagen/reuters.py
+++ neural_nlp/datagen/reuters.py
@@ -1,20 +1,20 @@
-"""
-Reuters
-"""
 import os
 import tarfile
 import warnings
 
 from glob import glob
 from bs4 import BeautifulSoup
-from sklearn.datasets import get_data_home
-from sklearn.externals.six.moves import urllib
 
-
+from neural_nlp.io import download_data
 
 
 class ReutersDataGen:
-    '''Utility class to parse a SGML file and yield documents one at a time.'''
+    """
+    Utility class to parse Reuters-21578 SGML file and iterate over the documents .
+    """
+
+    _data_name = 'reuters'
+    _download_url= 'http://archive.ics.uci.edu/ml/machine-learning-databases/reuters21578-mld/reuters21578.tar.gz'
 
     def __init__(self, split=None, data_path=None):
         splits = ['cgisplit', 'lewissplit', 'modapte', None]
@@ -23,13 +23,17 @@ class ReutersDataGen:
             split = 'unknown'
         self.split = split
         self.data_path = data_path
+        self.datagen = None
+
+    def __iter__(self):
+        self.datagen = self._data_gen()
+        self
+
+    def __next__(self):
+        return next(self.datagen)
 
 
     def is_training_doc(self, doc):
-        '''
-        :param doc:
-        :return:
-        '''
         is_train = False
         if self.split == 'cgisplit':
             if doc['cgisplit'] == 'TRAINING-SET':
@@ -43,10 +47,6 @@ class ReutersDataGen:
         return is_train
 
     def is_test_doc(self, doc):
-        '''
-        :param doc:
-        :return:
-        '''
         is_test = False
         if self.split == 'cgisplit':
             if doc['cgisplit'] == 'PUBLISHED-TESTSET':
@@ -60,10 +60,6 @@ class ReutersDataGen:
         return is_test
 
     def parse_doc(self, doc):
-        '''
-        :param doc:
-        :return:
-        '''
         data = {}
         text = doc.find('text')
         bt = text.find('bodytext')
@@ -83,10 +79,6 @@ class ReutersDataGen:
         return data
 
     def parse_file(self, file):
-        '''
-        :param file:
-        :return:
-        '''
         # hack: <body> tags not allowed
         sgm = open(file, 'r', encoding='latin1').read().replace('BODY>', 'BODYTEXT>')
         dom = BeautifulSoup(sgm, 'lxml')
@@ -104,55 +96,12 @@ class ReutersDataGen:
                 yield _doc
 
 
-    def data_dir(self):
-        '''Iterate over documents of the Reuters dataset.
-
-        The Reuters archive will automatically be downloaded and uncompressed if
-        the `data_path` directory does not exist.
-
-        Documents are represented as dictionaries with 'body' (str),
-        'title' (str), 'topics' (list(str)) keys.
-
-        '''
-
-        archive_filename = 'reuters21578.tar.gz'
-        download_url = '%s%s' % ('http://archive.ics.uci.edu/ml/machine-learning-databases/reuters21578-mld/',
-                                 archive_filename)
-        data_path = self.data_path
-        if data_path is None:
-            data_path = os.path.join(get_data_home(), 'reuters')
-        if not os.path.exists(data_path):
-            # Download the dataset.
-            print('downloading dataset (once and for all) into %s' % data_path)
-            os.mkdir(data_path)
-
-            def progress(blocknum, bs, size):
-                '''
-                :param blocknum:
-                :param bs:
-                :param size:
-                :return:
-                '''
-                total_sz_mb = '%.2f MB' % (size / 1e6)
-                current_sz_mb = '%.2f MB' % ((blocknum * bs) / 1e6)
-                print('\rdownloaded %s / %s' % (current_sz_mb, total_sz_mb),
-                      end='')
-
-            archive_path = os.path.join(data_path, archive_filename)
-            urllib.request.urlretrieve(download_url, filename=archive_path,
-                                       reporthook=progress)
-            print('\r', end='')
-            print('untarring Reuters dataset...')
-            tarfile.open(archive_path, 'r:gz').extractall(data_path)
-            print('done.')
-
-        return data_path
-
-    def data_gen(self):
-        for filename in glob(os.path.join(self.data_dir(), '*.sgm')):
+    def _data_gen(self):
+        data_dir = download_data.get_data(self._download_url, self._data_name, untar=True)
+        print(data_dir)
+        for filename in glob(os.path.join(data_dir, '*.sgm')):
             for doc in self.parse_file(filename):
                 if doc is None:
                     continue
                 yield doc
-
 
